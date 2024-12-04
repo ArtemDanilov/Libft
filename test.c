0d87d1750862fe8407253e67311b6544b9d9a8f9
ArtemDanilov <artemdanilow95@gmail.com>
2024-12-04 13:20:02 +0100

    Improve test file

--- test.c
+++ test.c
@@ -1,38 +1,45 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: artem <artem@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/04 11:44:42 by artem             #+#    #+#             */
-/*   Updated: 2024/12/04 12:18:03 by artem            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 #include "./ft_isdigit.c"
+#include "./ft_isalnum.c"
 
 int	ft_isdigit(int d);
+int	ft_isdigit(int c);
 
 char*	cond_st(int n)
 {
 	return n ? "true" : "false";
 }
 
+void test_func(char* text, char* func_name, int (*func)(int))
+{
+	char	chars[] = {'9', '0', '3', 'A', 'z', '*', ' ', '-'};
+	size_t	i;
+
+	i = 0;
+	printf("%s() \n-----------------------\n", func_name);
+	while (i < sizeof(chars))
+	{
+		printf("'%c' %s => %s\n", chars[i], text, cond_st(func(chars[i])));
+		i++;
+	}
+	printf("-----------------------\n");
+}
+
+/*
+ * ------------------------------
+ *        Tested functions       *
+ * ------------------------------
+ */
 void	test_isdigit(void) {
-	printf("ft_isdigit() \n ------------------ \n");
-	printf("'0' is digit: %s\n", cond_st(ft_isdigit('0')));
-	printf("'9' is digit: %s\n", cond_st(ft_isdigit('0')));
-	printf("'5' is digit: %s\n", cond_st(ft_isdigit('5')));
-	printf("'$' is digit: %s\n", cond_st(ft_isdigit('$')));
-	printf("'A' is digit: %s\n", cond_st(ft_isdigit('A')));
-	printf("'z' is digit: %s\n", cond_st(ft_isdigit('z')));
-	printf("'_' is digit: %s\n", cond_st(ft_isdigit(' ')));
-	printf("------------------ \n");
+	test_func("is digit", "ft_isdigit", ft_isdigit);
+}
+
+void	test_isalnum(void) {
+	test_func("is alpha or digit", "ft_isalnum", ft_isalnum);
 }
 
 int	main(void)
 {
 	test_isdigit();
+	test_isalnum();
 }
