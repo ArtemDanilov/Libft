15d68a852d9bd85b0fd3b70eeee803e6bbdbe2b2
ArtemDanilov <artemdanilow95@gmail.com>
2025-01-03 21:55:17 +0100

    Created ft_strtrim.c

---
+++ ft_strtrim.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adanilov <adanilov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/03 12:25:07 by adanilov          #+#    #+#             */
+/*   Updated: 2025/01/03 14:32:13 by adanilov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	is_in_set(char c, char const *set)
+{
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	size_t	start;
+	size_t	end;
+	size_t	len;
+	size_t	i;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && is_in_set(s1[start], set))
+		start++;
+	end = strlen(s1);
+	while (end > start && is_in_set(s1[end - 1], set))
+		end--;
+	len = end - start;
+	str = malloc(len * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s1[start + i];
+		i++;
+	}
+	return (str);
+}
