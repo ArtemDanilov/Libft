b8e29181ec3f4454461877a92abd99a33fc1ee7f
ArtemDanilov <artemdanilow95@gmail.com>
2025-01-05 18:45:25 +0100

    Correct libft.h

--- libft.h
+++ libft.h
@@ -1,33 +1,20 @@
-#ifndef LIBFT_
-#define LIBFT_
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artem <artem@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/05 18:44:40 by artem             #+#    #+#             */
+/*   Updated: 2025/01/05 18:44:40 by artem            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <ctype.h>
-#include <stdlib.h>
+#ifndef LIBFT_H
+# define LIBFT_H
 
-#include "./ft_isalpha.c"
-#include "./ft_isdigit.c"
-#include "./ft_isalnum.c"
-#include "./ft_isascii.c"
-#include "./ft_isprint.c"
-#include "./ft_strlen.c"
-#include "./ft_memset.c"
-#include "./ft_memcpy.c"
-#include "./ft_memmove.c"
-#include "./ft_bzero.c"
-#include "./ft_toupper.c"
-#include "./ft_tolower.c"
-#include "./ft_strlcpy.c"
-#include "./ft_strlcat.c"
-#include "./ft_strchr.c"
-#include "./ft_strrchr.c"
-#include "./ft_strncmp.c"
-#include "./ft_strnstr.c"
-#include "./ft_atoi.c"
-#include "./ft_calloc.c"
-#include "./ft_strdup.c"
-#include "./ft_substr.c"
-#include "./ft_strjoin.c"
-#include "./ft_strtrim.c"
+# include <ctype.h>
+# include <stdlib.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int d);
@@ -44,14 +31,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
-char 	*ft_strdup(const char *s);
+char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
+void	ft_bzero(void *s, size_t n);
 
 #endif
