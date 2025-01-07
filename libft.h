6f22826d011dd017351ae412c0c3d9d5629e8662
ArtemDanilov <artemdanilow95@gmail.com>
2025-01-07 10:02:15 +0100

    Add lost functions to libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: artem <artem@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 18:44:40 by artem             #+#    #+#             */
-/*   Updated: 2025/01/05 18:44:40 by artem            ###   ########.fr       */
+/*   Updated: 2025/01/07 09:56:16 by artem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *nptr);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
@@ -38,7 +39,8 @@ char	*ft_strtrim(char const *s1, char const *set);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memchr(const void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
